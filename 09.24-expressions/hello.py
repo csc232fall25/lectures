57c91b6c83481ad6561d25184f63743de888daf8
Christopher Siu <christopher@siu.sh>
2025-09-24 10:56:07 -0700

    Added example of a Python program.

--- 09.24-expressions/hello.py
+++ 09.24-expressions/hello.py
@@ -1,1 +1,7 @@
-print("Hello, world!")
+# NOTE: Lines like this that begin with a hash symbol are comments. They're
+#       ignored by the interpreter and they allow us to add explanatory text
+#       to our code. A program is then a sequence of both statements and
+#       comments, stored in a file that can be run over and over again.
+
+name = input("What is your name? ")
+print("Hello, " + name + "!")
