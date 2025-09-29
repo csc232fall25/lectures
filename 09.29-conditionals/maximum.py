7606aaf3145441a5fec21745877a5d9fdd4f191f
Christopher E. Siu <cesiu@calpoly.edu>
2025-09-29 11:12:34 -0700

    Added example of conditionals.

--- 09.29-conditionals/maximum.py
+++ 09.29-conditionals/maximum.py
@@ -2,4 +2,17 @@ x = int(input("What is an integer? "))
 y = int(input("What is another integer? "))
 z = int(input("What is another integer? "))
 
-print("The maximum is " + str(x) + ".")
+if x > y:
+    # NOTE: By themselves, a pair of nested "if..."s is essentially equivalent to
+    #       an "if...and...".
+    if x > z:
+        print("The maximum is " + str(x) + ".")
+    else:
+        print("The maximum is " + str(z) + ".")
+else:
+    # NOTE: But by splitting an "if...and..." into two nested "if..."s, we can
+    #       attach a different "else..." to each "if...".
+    if y > z:
+        print("The maximum is " + str(y) + ".")
+    else:
+        print("The maximum is " + str(z) + ".")
