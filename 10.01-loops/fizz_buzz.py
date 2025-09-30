0c9ad999bdca1d9d8ab31d263546dfe2a9020d7c
Christopher E. Siu <cesiu@calpoly.edu>
2025-09-30 08:36:00 -0700

    Added example of loops.

---
+++ 10.01-loops/fizz_buzz.py
@@ -0,0 +1,22 @@
+# TODO: Suppose the user types:
+#        3
+#        y
+#        0
+#       ...what will the following program print?
+
+done = False
+
+while not done:
+    limit = int(input("Iterations? "))
+
+    for i in range(1, limit + 1):
+        if i % 3 == 0 and i % 5 == 0:
+            print("fizz buzz")
+        elif i % 3 == 0:
+            print("fizz")
+        elif i % 5 == 0:
+            print("buzz")
+        else:
+            print(i)
+
+    done = (limit <= 0) or (input("Again? ") != "y")
