43bec01f0b16be085f5f8f9fb29ed920c2fb577c
Christopher E. Siu <cesiu@calpoly.edu>
2025-10-22 10:02:04 -0700

    Added base code for lists and references.

---
+++ 10.22-references/test.py
@@ -0,0 +1,19 @@
+def maximum(lst):
+    """
+    Find the largest element in a list.
+
+    :param lst: A non-empty list of integers
+    :return: The largest integer in the list
+    """
+    return None
+
+
+def scale(lst, scalar):
+    """
+    Multiply every element of a list by a scalar.
+
+    :param lst: A list of integers
+    :param scalar: An integer scalar
+    :return: A new list of the scaled elements
+    """
+    return []
