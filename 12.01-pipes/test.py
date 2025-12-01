d53339dab99b729f256f8b964d6ad36d67f3faf2
Christopher E. Siu <cesiu@calpoly.edu>
2025-12-01 10:49:36 -0800

    Added example of pipes.

--- 12.01-pipes/test.py
+++ 12.01-pipes/test.py
@@ -1,37 +1,50 @@
 import os
 import multiprocessing as mp
 
-# NOTE: Processes do not share memory -- generally speaking, for security, no
-#       process has access to any other process's data. Even if we put that
-#       data into global variables, it cannot be shared across processes.
-x = None
-y = None
 
-
-def min_element(lst):
-    global x
+def min_element(lst, pipe_end):
     x = min(lst)
     print("min:", os.getpid(), x)
+    pipe_end.send(x)
 
 
-def max_element(lst):
-    global y
+def max_element(lst, pipe_end):
     y = max(lst)
     print("max:", os.getpid(), y)
+    pipe_end.send(y)
 
 
 def main():
-    global x, y
+    # NOTE: Processes do not share memory -- generally speaking, for security,
+    #       no process has access to any other process's data. Even if we put
+    #       that data into global variables, it is not shared across processes.
+    # global x, y
 
     lst = [2, -1, 9, 8, 5, 0, 8, 4]
 
-    p1 = mp.Process(target=min_element, args=[lst])
-    p2 = mp.Process(target=max_element, args=[lst])
-    p1.start()
-    p2.start()
+    # NOTE: A pipe is essentially a temporary file managed by the OS: like a
+    #       file, a pipe is accessible by multiple processes; unlike a file,
+    #       a pipe can be stored in memory and is not publicly visible.
+    parent_end1, child_end1 = mp.Pipe()
 
-    p1.join()
-    p2.join()
+    # NOTE: If both children used the same pipe, we would have no way of
+    #       knowing which data was sent by which child -- generally speaking,
+    #       we need a separate pipe for each child.
+    parent_end2, child_end2 = mp.Pipe()
+
+    child1 = mp.Process(target=min_element, args=[lst, child_end1])
+    child2 = mp.Process(target=max_element, args=[lst, child_end2])
+    child1.start()
+    child2.start()
+
+    # NOTE: Any data sent into the pipes by the children can then be recevied
+    #       from the corresponding ends of the pipes within the parent. Here,
+    #       we receive before we join in order to get the data ASAP.
+    x = parent_end1.recv()
+    y = parent_end2.recv()
+
+    child1.join()
+    child2.join()
     print("main:", os.getpid(), x, y)
 
 
